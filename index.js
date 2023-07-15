540e7c815cccf72538a514e0e615f63372539042
Araragi Koyomi <araragikoyomi229@gmail.com>
2023-07-15 20:59:24 +0800

    update folder structure

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const otakudesu = require("./Routes/OtakudesuRoute");
-const komiku = require("./Routes/KomikuRoute");
+const otakudesu = require("./src/Routes/OtakudesuRoute");
+const komiku = require("./src/Routes/KomikuRoute");
 
 app.use("/otakudesu", otakudesu);
 app.use("/komiku", komiku);
